faebd24106716fe0e216bfa3544b6fead1fc9840
viniciusguigo <viniciusguigo@gmail.com>
2020-03-03 13:21:06 -0600

    finished example gui

--- gui/test_pyqtgraph.py
+++ gui/test_pyqtgraph.py
@@ -38,28 +38,33 @@ class GUI(object):
         view.resize(800,600)
 
         # add plot title
-        l.addLabel('The Cycle-of-Learning Experiment', col=0, colspan=1)
-        l.nextRow()
+        l.addLabel('The Cycle-of-Learning Experiment', col=0, colspan=4)
 
         # enable antialiasing for prettier plots
         pg.setConfigOptions(antialias=True)
 
         # add plot #1
-        p1 = l.addPlot(title="Total Reward per Episode")
+        l.nextRow()
+        p1 = l.addPlot(title="Total Reward per Episode", col=0, colspan=4)
         p1.setLabel('bottom', 'Episode')
         p1.setLabel('left', 'Total Reward')
         self.curve1 = p1.plot(pen=pg.mkPen('y', width=2))
 
         # add plot #2 on the next line
         l.nextRow()
-        p2 = l.addPlot(title="Expect Discounted Return")
+        p2 = l.addPlot(title="Expect Discounted Return", col=0, colspan=3)
         p2.setLabel('bottom', 'Time Step')
         p2.setLabel('left', 'Q(s,a)')
         self.curve2 = p2.plot(pen=pg.mkPen((80,0,0), width=3))
 
-        # # add bar plot besides plot #2
-        # bg = pg.BarGraphItem(x=1, height=100, width=0.3, brush='g')
-        # p3 = l.addPlot(title="Confidence")
+        # add bar plot besides plot #2
+        p3 = l.addPlot(title="Confidence", col=3)
+        p3.setLabel('left', '%')
+        p3.hideAxis('bottom')
+        p3.setRange(xRange=[-.5,1.5])
+        p3.setRange(yRange=[0, 100])
+        # p3.plot(np.array([0,1]), np.array([100]), stepMode=True, fillLevel=0, brush=(0,255,0,150))
+        self.curve3 = p3.plot(stepMode=True, fillLevel=0, brush=(0,255,0,150))
         
         # setup update
         timer = QtCore.QTimer()
@@ -73,6 +78,7 @@ class GUI(object):
         # read new values and update plot
         self.curve1.setData(self.datagen.xvals, self.datagen.yvals)
         self.curve2.setData(self.datagen.xvals, self.datagen.yvals)
+        self.curve3.setData(np.array([0,1]), [np.random.uniform(0, 100)])
 
 def init_gui(datagen):
     # Initialize GUI in a separate thread (not the main thread)
