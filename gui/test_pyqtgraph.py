4df2daf1cb28b4a00e07c1902a4436ecec6e40c0
viniciusguigo <viniciusguigo@gmail.com>
2020-03-03 09:02:55 -0600

    experimenting with gui layout

--- gui/test_pyqtgraph.py
+++ gui/test_pyqtgraph.py
@@ -30,17 +30,36 @@ class GUI(object):
         self.app = QtGui.QApplication([])
 
         # create window
-        win = pg.GraphicsWindow(title="Reinforcement Learning Experiment")
-        win.resize(1000,600)
+        view = pg.GraphicsView()
+        l = pg.GraphicsLayout(border=(100,100,100))
+        view.setCentralItem(l)
+        view.show()
+        view.setWindowTitle('pyqtgraph')
+        view.resize(800,600)
+
+        # add plot title
+        l.addLabel('The Cycle-of-Learning Experiment', col=0, colspan=1)
+        l.nextRow()
 
         # enable antialiasing for prettier plots
         pg.setConfigOptions(antialias=True)
 
-        # add plot
-        p6 = win.addPlot(title="Environment XYZ")
-        p6.setLabel('bottom', 'Episode')
-        p6.setLabel('left', 'Total Reward')
-        self.curve = p6.plot(pen='y')
+        # add plot #1
+        p1 = l.addPlot(title="Total Reward per Episode")
+        p1.setLabel('bottom', 'Episode')
+        p1.setLabel('left', 'Total Reward')
+        self.curve1 = p1.plot(pen=pg.mkPen('y', width=2))
+
+        # add plot #2 on the next line
+        l.nextRow()
+        p2 = l.addPlot(title="Expect Discounted Return")
+        p2.setLabel('bottom', 'Time Step')
+        p2.setLabel('left', 'Q(s,a)')
+        self.curve2 = p2.plot(pen=pg.mkPen((80,0,0), width=3))
+
+        # # add bar plot besides plot #2
+        # bg = pg.BarGraphItem(x=1, height=100, width=0.3, brush='g')
+        # p3 = l.addPlot(title="Confidence")
         
         # setup update
         timer = QtCore.QTimer()
@@ -52,7 +71,8 @@ class GUI(object):
 
     def _update(self):
         # read new values and update plot
-        self.curve.setData(self.datagen.xvals, self.datagen.yvals)
+        self.curve1.setData(self.datagen.xvals, self.datagen.yvals)
+        self.curve2.setData(self.datagen.xvals, self.datagen.yvals)
 
 def init_gui(datagen):
     # Initialize GUI in a separate thread (not the main thread)
